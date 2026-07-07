c63e6ead2e61b451cc9e0435910561b184c85b8c
Marios Kalantzis <marios.kalan@gmail.com>
2026-07-08 00:10:15 +0300

    Add initial HTML, CSS, and JavaScript structure for Etch-a-Sketch project

---
+++ index.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+for (let i = 0; i < 256; i++) {
+    const grid = document.createElement('div');
+    grid.classList.add('square');
+    container.appendChild(grid);
+}
