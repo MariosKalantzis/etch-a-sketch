5bd2b983b9d9aa48cb71565f80b188de5d17ea0f
Marios Kalantzis <marios.kalan@gmail.com>
2026-07-11 00:54:03 +0300

    Implement grid setup and resizing functionality with slider input

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
+const DEFAULT_SIZE = 50;
+
+let currentSize = DEFAULT_SIZE;
+
 const container = document.querySelector('.container');
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
-    square.addEventListener('mouseenter', () => {
-        square.style.backgroundColor = 'black';
-    });
+const resizeButton = document.querySelector('.resize-button');
+const sizeValue = document.getElementById('sizeValue');
+const sizeSlider = document.getElementById("sizeSlider");
+
+const setupGrid = (size) => {
+    for (let i = 0; i < size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = 'black';
+        });
+    }
+
 }
+
+
+console.log(sizeSlider);
+const updateSizeValue = (value) => {
+    sizeValue.textContent = `${value} x ${value}`;
+}
+
+const setCurrentSize = (newSize) => {
+    currentSize = newSize;
+}
+
+const changeSize = (value) => {
+    setCurrentSize(value);
+    updateSizeValue(value);
+    resetGrid();
+    setupGrid(currentSize);
+}
+
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+
+
+window.onload = () => {
+    setupGrid(DEFAULT_SIZE);
+}
+
+const resetGrid = () => {
+    container.textContent = ''
+}
+
