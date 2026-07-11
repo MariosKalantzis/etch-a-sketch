3d84b523bc645bbce975adae26fc47a0c09b04ed
Marios Kalantzis <marios.kalan@gmail.com>
2026-07-11 21:33:36 +0300

    Refactor clear button functionality and update grid setup for dynamic square sizing

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const DEFAULT_SIZE = 50;
 let currentSize = DEFAULT_SIZE;
 
 const container = document.querySelector('.container');
-const resizeButton = document.querySelector('.resize-button');
+const clearButton = document.querySelector('.clear-button');
 const sizeValue = document.getElementById('sizeValue');
 const sizeSlider = document.getElementById("sizeSlider");
 
 const setupGrid = (size) => {
-    for (let i = 0; i < size; i++) {
+    const squareSize = 960 / size;
+    for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.style.width = squareSize + `px`;
+        square.style.height = squareSize + `px`;
         container.appendChild(square);
         square.addEventListener('mouseenter', () => {
             square.style.backgroundColor = 'black';
@@ -48,3 +51,7 @@ const resetGrid = () => {
     container.textContent = ''
 }
 
+clearButton.addEventListener('click', () => {
+    resetGrid();
+    setupGrid(currentSize);
+})
