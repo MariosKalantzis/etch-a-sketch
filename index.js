525d52286b72562fb8ae1364868115d0ce7a7252
Marios Kalantzis <marios.kalan@gmail.com>
2026-07-12 13:37:51 +0300

    Add color palette input and adjust canvas size calculations

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const DEFAULT_SIZE = 50;
 
 let currentSize = DEFAULT_SIZE;
 
+const CANVAS_SIZE = 500;
+
+const DEFAULT_COLOR = "black";
+
+let currentColor = DEFAULT_COLOR;
+
+
 const container = document.querySelector('.container');
+container.style.width = CANVAS_SIZE + 'px';
+container.style.height = CANVAS_SIZE + 'px';
+
 const clearButton = document.querySelector('.clear-button');
 const sizeValue = document.getElementById('sizeValue');
 const sizeSlider = document.getElementById("sizeSlider");
+const colorPallet = document.getElementById('colorPallet');
+
+
 
 const setupGrid = (size) => {
-    const squareSize = 960 / size;
+    const squareSize = CANVAS_SIZE / size;
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
@@ -39,8 +52,14 @@ const changeSize = (value) => {
     setupGrid(currentSize);
 }
 
+const updateColor = (value) => {
+    currentColor = value;
+}
+
 sizeSlider.onchange = (e) => changeSize(e.target.value);
 sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+colorPallet.onchange = (e) => updateColor(e.target.value);
+
 
 
 window.onload = () => {
